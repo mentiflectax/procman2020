a15724d1a9e7e9228846d42e2cbe9f79e8dce1f5
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-05-06 12:48:38 +0200

    Minimal Camunda is ready.

---
+++ src/main/java/com/dpisarenko/procman2020/ProcMan2020App.java
@@ -0,0 +1,11 @@
+package com.dpisarenko.procman2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcMan2020App {
+    public static void main(String... args) {
+        SpringApplication.run(ProcMan2020App.class, args);
+    }
+}
