fb3a1f2fc808245e84f8e425fe17bea9d1e4331c
Dmitrii Pisarenko <dp@dpisarenko.com>
2021-05-07 14:07:19 +0000

    Merged in i2 (pull request #2)
    I-2: Connection to PostgresQL
    
    * I-2: Trying to make it work with PostgresQL.
    
    * I-2: Trying to make it work with PostgresQL.
    
    * I-2: Trying to make it work with PostgresQL.
    
    * I-2: It seems to work now.
    
    * I-3: Ready for deployment.

---
+++ src/main/java/com/dpisarenko/procman2020/CamundaDb.java
@@ -0,0 +1,40 @@
+package com.dpisarenko.procman2020;
+
+import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
+import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration;
+import org.camunda.bpm.spring.boot.starter.util.SpringBootProcessEnginePlugin;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableConfigurationProperties
+public class CamundaDb {
+    @Bean(name = "camundaBpmDataSource")
+    @ConfigurationProperties(prefix = "com.camunda.datasource")
+    public DataSource dsCamunda() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean("camundaTxManager")
+    public PlatformTransactionManager camundaTransactionManager(@Qualifier("camundaBpmDataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public ProcessEnginePlugin fixCamundaTransactionManager(@Qualifier("camundaTxManager") PlatformTransactionManager camundaTxManager) {
+        return new SpringBootProcessEnginePlugin() {
+            @Override
+            public void preInit(SpringProcessEngineConfiguration configuration) {
+                configuration.setTransactionManager(camundaTxManager);
+            }
+        };
+    }
+}
